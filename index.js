9e1cb6e75a15b54d16bd19d098e874a71b208187
JacobPCM <jacobd@postcardmania.com>
2023-10-10 17:20:13 -0400

    Configure non-operator buttons

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
+function updateOutput(digit) {
+    const output = document.querySelector('.output');
+    output.textContent += digit;
+}
+
+function clearOutput() {
+    const output = document.querySelector('.output');
+    output.textContent = '';
+}
+
+function addDecimal() {
+    const output = document.querySelector('.output');
+    if(output.textContent.includes('.')){
+        return;
+    } else {
+        output.textContent += '.';
+    }
+}
+
+function changeSign() {
+    const output = document.querySelector('.output');
+    if(output.textContent.includes('-')){
+        output.textContent = output.textContent.slice(1);
+    } else {
+        output.textContent = '-' + output.textContent;
+    }
+}
+
+const calcButtons = document.querySelectorAll('.button')
+console.log(calcButtons)
+
+//Configure actions for each button
+calcButtons.forEach(calcButton => calcButton.addEventListener('click', e => {
+    if(e.target.innerText >= 0 && e.target.innerText <= 9) {
+        updateOutput(e.target.innerText)
+    }
+
+    if(e.target.innerText === 'AC') {
+        clearOutput();
+    }
+
+    if(e.target.innerText === '.') {
+        addDecimal();
+    }
+
+    if(e.target.innerText === '+/-') {
+        changeSign();
+    }
+}))
+
 
